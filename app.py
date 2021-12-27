813eb98785135207b95481a91a0d4a5f3b2975ec
Sonakshi <sonakshi2500@gmail.com>
2021-12-28 00:11:25 +0530

    .

--- app.py
+++ app.py
@@ -6,7 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-
+@app.route('/registration')
+def register():
+    return render_template("registrations.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
