9174ed388ec2ee8469377e331ac003c7cc4f4398
Sonakshi <sonakshi2500@gmail.com>
2021-12-29 17:21:41 +0530

    .

--- app.py
+++ app.py
@@ -22,5 +22,36 @@ def index():
 def register():
     return render_template("registrations.html")
 
+@app.route('/registrations2')
+def register2():
+    return render_template("registrations2.html")
+
+@app.route('/add-user', methods = ['POST'])
+def addUser():
+    try :
+        teamType=request.form.get('teamType')
+        members=request.form.get('members')
+        name = request.form.get('name')
+        email = request.form.get('email')
+        phone = request.form.get('phone')
+        age = request.form.get('age')
+        education = request.form.get('education')
+        state = request.form.get('state')
+
+        if name and email and phone and age and education and state:
+
+            if user.query.filter_by(email=email).first():
+                return("Email ID already Exists")
+            else:
+                db.session.add(user(name=name, email=email, phone=phone, age=age, education=education, state=state, avatar_id=avatar))
+                db.session.commit()
+                return("Thank you for registering")
+        else:
+            return("Please fill all the fields")
+    except Exception as e:
+        print(e)
+        return("Something went wrong")
+
+
 if __name__ == '__main__':
     app.run(debug=True)
