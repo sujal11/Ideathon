4cc1f5ab2d3ddf4a892e0f13abb5f0eebc4121d3
Sonakshi <sonakshi2500@gmail.com>
2021-12-29 22:17:19 +0530

    .

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import json
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI']='postgresql://sonakshi:sonakshi@localhost/ideathon'
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql:///ideathon'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['DEBUG'] = True
 db = SQLAlchemy(app)
