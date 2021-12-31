344ac9b1fc4e5006a3591439e424c4054b7afd7f
Sonakshi <sonakshi2500@gmail.com>
2021-12-31 12:52:57 +0530

    .

--- app.py
+++ app.py
@@ -56,33 +56,59 @@ def addUser():
         print(e)
         return("Something went wrong")
 
-@app.route('/add-teamates', methods = ['POST','GET'])
-def addteamates():
-    try :
-        members=int(request.form.get('members'))
-        team_member_name=[]
-        team_member_email=[]
-        team_member_phone=[]
-        for i in range(members):
-            team_member_name[i]=request.form.get('member_name'+str(i))
+# @app.route('/add-teamates', methods = ['POST','GET'])
+# def addteamates():
+#     try :
+#         members=int(request.form.get('members'))
+#         team_member_name=[]
+#         team_member_email=[]
+#         team_member_phone=[]
+#         for i in range(members):
+#             team_member_name[i]=request.form.get('member_name'+str(i))
 
-        if teamType and members and tname and tlname and tlemail and tlphone and organisation and problem_statement:
+#         if teamType and members and tname and tlname and tlemail and tlphone and organisation and problem_statement:
 
-            if participants.query.filter_by(email=tlemail).first():
-                return("Email ID already Exists")
-            else:
-                db.session.add(team(team_name=tname, team_members=members, team_type=teamType, problem_statement=problem_statement))
-                db.session.commit()
-                teams=team.query.filter_by(team_name=tname).first()
-                db.session.add(participants(name=tlname, email=tlemail, phone=tlphone, organization=organisation, team_name=tname,is_leader=True, team_id=teams.id))
-                db.session.commit()
-                return ("true")
-        else:
-            return("Please fill all the fields")
-    except Exception as e:
-        print(e)
-        return("Something went wrong")
+#             if participants.query.filter_by(email=tlemail).first():
+#                 return("Email ID already Exists")
+#             else:
+#                 db.session.add(team(team_name=tname, team_members=members, team_type=teamType, problem_statement=problem_statement))
+#                 db.session.commit()
+#                 teams=team.query.filter_by(team_name=tname).first()
+#                 db.session.add(participants(name=tlname, email=tlemail, phone=tlphone, organization=organisation, team_name=tname,is_leader=True, team_id=teams.id))
+#                 db.session.commit()
+#                 return ("true")
+#         else:
+#             return("Please fill all the fields")
+#     except Exception as e:
+#         print(e)
+#         return("Something went wrong")
 
+@app.route('/attendees')
+def attendees():
+    teams=team.query.all()
+    participant=participants.query.all()
+    usr_list = []
+
+    for u in teams:
+        usr = {
+                    'id': u.id,
+                    'team_name': u.team_name,
+                    'team_members': u.team_members,
+                    'team_type': u.team_type,
+                    'problem_statement': u.problem_statement,
+                    }
+        i=1
+        for v in participant:
+            if u.id==v.team_id:
+                usr['participant_name'+str(i)]=v.name
+                usr['participant_email'+str(i)]=v.email
+                usr['participant_phone'+str(i)]=v.phone
+                usr['participant_organization'+str(i)]=v.organization
+                usr['participant_is_leader'+str(i)]=v.is_leader
+            i=i+1
+        print(usr)
+        usr_list.append(usr)
+    return render_template("attendees.html",users=usr_list)
 
 if __name__ == '__main__':
     app.run(debug=True)
